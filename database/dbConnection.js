6a221167dea5b34431d5c99c5b342656625b49c1
Mohammed Madkour <mohamedmadkour312@gmai.com>
2024-12-11 13:25:43 +0200

    initial commit

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export const dbConnection = () => {
   mongoose
-    .connect("mongodb://127.0.0.1:27017/FreshCart-e-commerce")
+    .connect("mongodb://127.0.0.1:27017/FreshCart-e-commerce", {
+      serverSelectionTimeoutMS: 20000,
+    })
     .then(() => console.log("dataBase Connected"))
     .catch(() => console.log("dataBase didn't connect successfully"));
 };
