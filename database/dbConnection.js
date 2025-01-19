991a160e84a4c811edba7f899cdffc08fbde2b54
Mohammed Madkour <mohamedmadkour312@gmai.com>
2025-01-19 15:44:06 +0200

    initial comit

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -3,8 +3,10 @@ import mongoose from "mongoose";
 export const dbConnection = () => {
   mongoose
     .connect(
-      "mongodb+srv://mohamedmadkour312:<m2291672>@cluster0.ekftryb.mongodb.net/"
+      "mongodb+srv://mohamedmadkour312:<yJL7N6OTdsGJFQVi>@cluster0.ekftryb.mongodb.net/"
     )
     .then(() => console.log("dataBase Connected"))
     .catch(() => console.log("dataBase didn't connect successfully"));
 };
+
+// password : yJL7N6OTdsGJFQVi
