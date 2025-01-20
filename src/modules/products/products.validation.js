bb0766dcbec88a33ce6f17eed67fb867fb80c06a
Mohammed Madkour <mohamedmadkour312@gmai.com>
2025-01-20 12:03:52 +0200

    initial commit

--- src/modules/products/products.validation.js
+++ src/modules/products/products.validation.js
@@ -3,6 +3,7 @@ import Joi from "joi";
 const addProductValidation = Joi.object({
   title: Joi.string().min(3).max(500).required(),
   price: Joi.number().min(0).required(),
+  priceAfterDisscount: Joi.number().min(0),
   description: Joi.string().min(0).required(),
   sold: Joi.number().min(0),
   quantity: Joi.number().min(0),
