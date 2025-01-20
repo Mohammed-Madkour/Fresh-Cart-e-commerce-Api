5ddc4698dff421af920f2636544495e0ac316b7f
Mohammed Madkour <mohamedmadkour312@gmai.com>
2025-01-20 07:41:33 +0200

    initial commit

--- src/modules/products/products.validation.js
+++ src/modules/products/products.validation.js
@@ -23,11 +23,11 @@ const updateProductValidation = Joi.object({
   quantity: Joi.number().min(0),
   images: Joi.array(),
   imageCover: Joi.string(),
-  ratingsQuantity: Joi.number(),
+  ratingsQuantity: Joi.number().min(0),
   categoryId: Joi.string(),
   brandId: Joi.string(),
   subCategoryId: Joi.string(),
-  ratingAvg: Joi.number(),
+  ratingAvg: Joi.number().min(0),
   id: Joi.string().hex().length(24).required(),
 });
 const deleteProductValidation = Joi.object({
